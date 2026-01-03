1db1624581f9494a359d7d2f12e0b38b936f3d3c
SevenMatt <matheusmamedd@gmail.com>
2026-01-03 11:35:55 -0300

    Atualização na Migration

---
+++ db/migrate/20260103142932_remove_duplicate_email_from_users.rb
@@ -0,0 +1,5 @@
+class RemoveDuplicateEmailFromUsers < ActiveRecord::Migration[8.0]
+  def change
+    remove_column :users, :email, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_02_213815) do
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+ActiveRecord::Schema[8.0].define(version: 2026_01_03_142932) do
+  create_table "appointments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_appointments_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "appointments", "users"
 end
